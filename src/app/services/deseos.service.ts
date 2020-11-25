3ea315a586026c9568afbba9378be2a2ac9cb8bb
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-24 23:44:03 -0600

    125 Localstorage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -6,13 +6,16 @@ import { Lista } from '../models/lista.model';
 })
 export class DeseosService {
 
+  
+
   listas: Lista[] = [];  
 
   constructor() { 
-    const lista1 = new Lista('Recolectar Piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
+    this.cargarStorage();
+    //const lista1 = new Lista('Recolectar Piedras del infinito');
+    //const lista2 = new Lista('Heroes a desaparecer');
 
-    this.listas.push(lista1, lista2);
+    //this.listas.push(lista1, lista2);
 
     
     
@@ -22,6 +25,23 @@ export class DeseosService {
   crearLista(titulo: string){
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+      localStorage.setItem('data', JSON.stringify (this.listas));
+  }
+
+  cargarStorage(){
+  
+    if(localStorage.getItem('data')){
+      this.listas = JSON.parse( localStorage.getItem('data'));
+    }else{
+      this.listas = [];
+    }
+
+    
+  
   }
   
 
